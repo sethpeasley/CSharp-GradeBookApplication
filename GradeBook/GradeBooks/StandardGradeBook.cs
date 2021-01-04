843821836a6da5912050cc29596a0374ea3cae04
Seth Peasley <seth.peasley@gmail.com>
2021-01-03 22:05:25 -0500

    Updated whole class; apparently the last version didn't get saved.

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,7 +1,19 @@
-namespace .
+using System;
+using System.Linq;
+
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GradeBook.GradeBooks
 {
-    public class StandardGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
-        
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
     }
 }
